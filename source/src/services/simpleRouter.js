9bdfc1101f49b1508cd6a271ed1286efa10c3a60
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-06-20 18:46:55 +0800

    add base in router and re-build

--- source/src/services/simpleRouter.js
+++ source/src/services/simpleRouter.js
@@ -1,9 +1,9 @@
 const routes = {
-	"/": {
+	"/ColorThemeGenerator/": {
 		title: document.title,
 		pageId: "home"
 	},
-	"/generate": {
+	"/ColorThemeGenerator/generate": {
 		title: "Generate - " + document.title,
 		pageId: "generate"
 	}
