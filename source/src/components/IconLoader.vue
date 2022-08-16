470cfd6421de3815d46bc1d9e20b35e59789aa31
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-08-16 20:45:54 +0800

    fixing IconLoader.vue: load icon dynamically

---
+++ source/src/components/IconLoader.vue
@@ -0,0 +1,20 @@
+<style scoped>
+svg {
+	display: inline-block;
+	width: 1em;
+	height: 1em;
+	overflow: visible;
+	font-size: inherit;
+	vertical-align: -.125em;
+}
+</style>
+<script setup>
+import { defineAsyncComponent } from "vue";
+
+const props = defineProps({ icon: String });
+const icon = props.icon.split("-").map(item => item[0].toUpperCase() + item.slice(1)).join("");
+const currentIcon = defineAsyncComponent(() => import(`./icons/${ icon }.vue`));
+</script>
+<template>
+	<component :is="currentIcon" />
+</template>
