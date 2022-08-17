381e40cacadb18131103a430d88889f75376a980
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-08-17 14:53:46 +0800

    update home style

--- source/src/components/PageHome.vue
+++ source/src/components/PageHome.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, reactive, onMounted, onUnmounted } from "vue";
 import { ColorSwatchIcon, ChevronDownIcon } from "@heroicons/vue/solid";
+import IconSocial from "./IconLoader.vue";
 
 const heroWrapper = ref(null);
 const hero = ref(null);
@@ -85,8 +86,8 @@ onUnmounted(() => window.removeEventListener("scroll", heroOnScroll));
 		</div>
 	</header>
 	<article>
-		<section class="container pt-32 pb-64">
-			<div class="flex p-4 md:px-24 justify-center items-center">
+		<section class="container">
+			<div class="flex p-4 md:px-24 justify-center items-center mt-32">
 				<div ref="heroWrapper" class="hero-wrapper relative w-full lg:w-2/3 rounded-2xl lg:rounded-md overflow-hidden opacity-90">
 					<div ref="hero" class="hero absolute w-full left-0 top-0 rounded-b-2xl lg:rounded-b-md" :style="{ height: data.heroHeight + 'px' }">&nbsp;</div>
 				</div>
@@ -96,6 +97,16 @@ onUnmounted(() => window.removeEventListener("scroll", heroOnScroll));
 				<div class="flex flex-col justify-center items-center">
 					<p class="text-lg text-gray-700 text-center md:leading-8 md:w-2/3 mb-4">Hi! I will support you to build great <strong>Palette Theme</strong>. Create your own palette with more shades you can customize and apply to your apps. You can get the result as <strong>CSS Color Custom Properties</strong> (--*) or <strong><i>tailwind.config.js</i></strong> if you are familiar with  Tailwind CSS.</p>
 					<a href="#generate" class="px-8 py-3 border border-transparent rounded-md shadow-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">New Theme</a>
+				</div>
+			</div>
+			<div class="flex justify-center mt-16 lg:mt-32 mb-32 lg:mb-48">
+				<div class="relative border rounded-md border-gray-300">
+					<a href="https://github.com/MuhammadSabri1306/ColorThemeGenerator" class="inline-flex items-center whitespace-nowrap px-4 py-3 text-xl hover:text-2xl text-gray-800 hover:text-indigo-700 transition-all duration-200 ease-in-out">
+						<IconSocial icon="solid-git-alt" class="ml-auto mr-1" />
+						<span class="inline-block md:hidden font-semibold text-base">https://github.com/MuhammadS...</span>
+						<span class="hidden md:inline-block font-semibold text-base">https://github.com/MuhammadSabri1306/ColorThemeGenerator</span>
+					</a>
+					<span class="absolute left-4 -top-5 p-2 text-xs font-semibold bg-gray-100 text-gray-600">Repository</span>
 				</div>
 			</div>
 		</section>
