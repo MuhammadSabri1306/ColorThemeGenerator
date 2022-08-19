cae8fb1d2bbcfd76927fa064de0a109edede1581
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-08-19 16:09:38 +0800

    s

--- source/tests/sample.test.js
+++
@@ -1,9 +0,0 @@
-export default {
-	/*log: () => {
-		return 2;
-	}*/
-	expected(){
-		return 2;
-	},
-	toBe: 1
-};
--- source/tests/searchAlgorithm.test.js
+++
@@ -1,8 +0,0 @@
-import searchAlgorithm from "../src/components/modules/searchAlgorithm";
-import tailwindPalette from "../src/store/data/tailwindPalette";
-
-const log = () => {
-	return searchAlgorithm("ind", tailwindPalette);
-};
-
-export default { log };
