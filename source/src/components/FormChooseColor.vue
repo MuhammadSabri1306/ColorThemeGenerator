6d5bb32d65732135470bd78fa8768c9d8aa43a0d
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-09-07 00:13:50 +0800

    update FormChooseColor.vue

--- source/src/components/FormChooseColor.vue
+++ source/src/components/FormChooseColor.vue
@@ -30,21 +30,6 @@ const setActiveTabs = tabsName => {
 
 const value = computed(() => store.getters["colorPicker/hex"]);
 
-onBeforeMount(() => {
-	const defaultHexColor = props.defaultValue ? props.defaultValue : "#ff0000";
-	store.commit("colorPicker/setHex", defaultHexColor);
-});
-
-const chooseTailwindColor = twColor => {
-	store.commit("colorPicker/setHex", twColor.color);
-	selectedTwName.value = twColor.name;
-};
-
-const chooseCustomColor = hexColor => {
-	store.commit("colorPicker/setHex", hexColor);
-	selectedTwName.value = null;
-};
-
 const submit = () => {
 	const val = store.getters["colorPicker/hex"];
 	if(val.length <= 0)
@@ -53,6 +38,22 @@ const submit = () => {
 	store.commit("colorPicker/setHex", "#ff0000");
 	emit("submit", val);
 };
+
+const chooseTailwindColor = twColor => {
+	store.commit("colorPicker/setHex", twColor.color);
+	selectedTwName.value = twColor.name;
+	submit();
+};
+
+const chooseCustomColor = hexColor => {
+	store.commit("colorPicker/setHex", hexColor);
+	selectedTwName.value = null;
+};
+
+onBeforeMount(() => {
+	const defaultHexColor = props.defaultValue ? props.defaultValue : "#ff0000";
+	store.commit("colorPicker/setHex", defaultHexColor);
+});
 
 onMounted(() => {
 	if(getActiveTabs == "customColor" && value.length < 1)
@@ -66,9 +67,11 @@ onMounted(() => {
 				<button @click="setActiveTabs('twPalette')" type="button" class="px-4 py-2 border border-indigo-600 rounded-l-md shadow-sm text-sm font-medium whitespace-nowrap overflow-hidden text-ellipsis hover:bg-indigo-700 hover:border-indigo-700 hover:text-white focus-shadow-default" :class="{ 'bg-indigo-600 text-white': getActiveTabs() == 'twPalette', 'bg-gray-100 text-indigo-600': getActiveTabs() == 'customColor' }" data-tabindex="2">Tailwind Palette</button>
 				<button @click="setActiveTabs('customColor')" type="button" class="px-4 py-2 border border-indigo-600 rounded-r-md shadow-sm text-sm font-medium whitespace-nowrap overflow-hidden text-ellipsis hover:bg-indigo-700 hover:border-indigo-700 hover:text-white focus-shadow-default" :class="{ 'bg-indigo-600 text-white': getActiveTabs() == 'customColor', 'bg-gray-100 text-indigo-600': getActiveTabs() == 'twPalette' }" data-tabindex="3">Custom Color</button>
 			</div>
-			<div>
-				<DataList v-if="getActiveTabs() == 'twPalette'" :model="tailwindPaletteList" @change="chooseTailwindColor" class="mb-8" />
-				<InputTypeColor v-else class="mb-4 px-8" :defaultValue="value" @change="chooseCustomColor" />
+			<div v-if="getActiveTabs() == 'twPalette'">
+				<DataList :model="tailwindPaletteList" @change="chooseTailwindColor" class="mb-8" />
+			</div>
+			<div v-else>
+				<InputTypeColor class="mb-4 px-8" :defaultValue="value" @change="chooseCustomColor" />
 				<div class="flex justify-end px-8">
 					<button @click="submit" :disabled="value.length < 1" type="button" class="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus-ring-default focus:ring-indigo-600" data-tabindex="1">Submit</button>
 				</div>
