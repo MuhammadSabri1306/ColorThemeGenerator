f10bdf97aad574c3bd80a2d1df610bc65f7ae19b
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-09-06 23:20:32 +0800

    update style for disabled button

--- source/src/components/FormSelectColorKey.vue
+++ source/src/components/FormSelectColorKey.vue
@@ -18,7 +18,7 @@ const formSelectList = computed(() => props.keyOption.map(({ val, disabled }) =>
 			<FormSelect :value="key" title="Select color key" :list="formSelectList" @change="(val) => key = val" class="custom-form-select" />
 		</div>
 		<div class="flex justify-end px-8">
-			<button type="button" class="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none" :disabled="key.length < 1" @click="$emit('submit', key)">Choose Color</button>
+			<button type="button" class="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 enabled:hover:bg-indigo-700 focus:outline-none" :disabled="key.length < 1" @click="$emit('submit', key)">Choose Color</button>
 		</div>
 	</FixedModal>
 </template>
