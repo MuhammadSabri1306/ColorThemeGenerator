3d7755ffd1e3028841c16a3bcc167ad2742cef61
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-09-07 16:30:29 +0800

    update InputTypeColor/Picker.vue: fixing touch event

--- source/src/components/custom-forms/InputTypeColor/Picker.vue
+++ source/src/components/custom-forms/InputTypeColor/Picker.vue
@@ -1,25 +1,28 @@
 <script setup>
-import { ref, computed, watch, onBeforeMount } from "vue";
+import { ref, computed, onMounted, onUnmounted } from "vue";
 
 const props = defineProps({
 	hue: Number,
-	left: Number,
-	top: Number
+	left: { type: Number, default: 100 },
+	top: { type: Number, default: 100 }
 });
 
 const emit = defineEmits(["change"]);
-
-const hue = ref(props.hue);
-const pickerLeft = ref(null);
-const pickerTop = ref(null);
+const hue = computed(() => props.hue);
+const pickerLeft = ref(props.left);
+const pickerTop = ref(props.top);
 const isPickerDrag = ref(false);
-const colorBox = ref(null);
 
-watch(() => props.hue, h => hue.value = h);
+const colorBoxElm = ref(null);
+const colorBox = computed(() => {
+	const width = !colorBoxElm.value ? 0 : colorBoxElm.value.offsetWidth,
+		height = !colorBoxElm.value ? 0 : colorBoxElm.value.offsetHeight,
+		left = !colorBoxElm.value ? 0 : colorBoxElm.value.offsetLeft,
+		top = !colorBoxElm.value ? 0 : colorBoxElm.value.offsetTop;
+	const right = left + width,
+		bottom = top + height;
 
-onBeforeMount(() => {
-	pickerLeft.value = props.left;
-	pickerTop.value = props.top;
+	return { width, height, left, top, right, bottom };
 });
 
 const bgGradient = computed(() => {
@@ -45,27 +48,58 @@ const movePicker = (x, y) => {
 	emit("change", { left: pickerLeft.value, top: pickerTop.value });
 };
 
+const isTouchEvent = event => ["touchstart", "touchmove", "touchend"].indexOf(event.type) >= 0;
+
+const getCursorPost = event => {
+	if(!isTouchEvent(event))
+		return { x: event.clientX, y: event.clientY };
+
+	const evt = (typeof event.originalEvent === 'undefined') ? event : event.originalEvent;
+    const touch = evt.touches[0] || evt.changedTouches[0];
+    return { x: touch.pageX, y: touch.pageY };
+};
+
 const startDragPicker = event => {
 	isPickerDrag.value = true;
-	colorBox.value = event.target.getBoundingClientRect();
-	
-	movePicker(event.clientX, event.clientY);
+	const { x, y } = getCursorPost(event);
+	movePicker(x, y);
 };
 
 const endDragPicker = event => {
 	isPickerDrag.value = false;
-	movePicker(event.clientX, event.clientY);
+	const { x, y } = getCursorPost(event);
+	movePicker(x, y);
 };
 
-const dragPicker = event => isPickerDrag.value && movePicker(event.clientX, event.clientY);
+const dragPicker = event => {
+	if(!isPickerDrag.value)
+		return;
+	const { x, y } = getCursorPost(event);
+	movePicker(x, y);
+};
 
-document.body.addEventListener("mousemove", dragPicker);
-document.body.addEventListener("touchmove", dragPicker);
-document.body.addEventListener("mouseup", endDragPicker);
-document.body.addEventListener("touchend", endDragPicker);
+onMounted(() => {
+	document.body.addEventListener("mousemove", dragPicker);
+	document.body.addEventListener("mouseup", endDragPicker);
+	document.body.addEventListener("touchmove", dragPicker);
+	document.body.addEventListener("touchend", endDragPicker);
+
+	if(document.body.style.overflow == "auto")
+		document.body.style.overflow = "hidden";
+});
+
+onUnmounted(() => {
+	document.body.removeEventListener("mousemove", dragPicker);
+	document.body.removeEventListener("mouseup", endDragPicker);
+	document.body.removeEventListener("touchmove", dragPicker);
+	document.body.removeEventListener("touchend", endDragPicker);
+
+	if(document.body.style.overflow == "hidden")
+		document.body.style.overflow = "auto";
+});
 </script>
 <template>
-	<div @mousedown="startDragPicker" @touchstart="startDragPicker" class="relative aspect-[2/1] cursor-move mb-8 before:absolute before:w-full before:h-full before:top-0 before:left-0 before:bg-gradient-to-t before:from-black before:to-transparent" :style="{ background: bgGradient }">
+	<div ref="colorBoxElm" @mousedown="startDragPicker" @touchstart="startDragPicker" class="relative aspect-[2/1] cursor-move mb-8 before:absolute before:w-full before:h-full before:top-0 before:left-0 before:bg-gradient-to-t before:from-black before:to-transparent" :style="{ background: bgGradient }">
 		<div class="absolute w-4 h-4 border-2 border-gray-100 rounded-full shadow-[0_0_0.5rem_rgba(0,0,0,0.4)]" :style="pickerPostStyles"></div>
 	</div>
 </template>
